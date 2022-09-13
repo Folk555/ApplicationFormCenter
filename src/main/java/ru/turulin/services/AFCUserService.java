a43c19be47d169ba40601595e2f3915f85a0ecc5
Folk555 <sanya.turulin.98@list.ru>
2022-09-13 11:53:11 +0300

    Доработан AFCUserService

--- src/main/java/ru/turulin/services/AFCUserService.java
+++ src/main/java/ru/turulin/services/AFCUserService.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.turulin.exeptions.ExistingElementInDataSourceException;
 import ru.turulin.exeptions.NotFoundElementInDataSourceException;
 import ru.turulin.models.Account;
@@ -13,8 +18,8 @@ import ru.turulin.models.Role;
 import ru.turulin.repos.AccountRepo;
 import ru.turulin.repos.PersonalityRepo;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @PropertySource("classpath:urls.properties")
@@ -51,7 +56,6 @@ public class AFCUserService {
         return personality;
     }
 
-
     public void activateEmail(String UUIDcode) throws NotFoundElementInDataSourceException {
         Personality personality = personalityRepo.findByActivateCode(UUIDcode);
         if (personality == null) {
@@ -61,5 +65,43 @@ public class AFCUserService {
         personalityRepo.save(personality);
     }
 
+
+
+
+    public Iterable<Account> getAllAccounts() {
+        return accountRepo.findAll();
+    }
+
+    public Account getAccountById(long accountId) throws NotFoundElementInDataSourceException {
+        Optional<Account> optionalAccount = accountRepo.findById(accountId);
+        if (!optionalAccount.isPresent())
+            throw new NotFoundElementInDataSourceException("Account by ID not found in database.");
+        return optionalAccount.get();
+    }
+
+    /**
+     * @param account
+     * @param setRoles - Множество названий ролей.
+     * @return
+     */
+    public Account updateAccount(Account account, Set<String> setRoles) throws NotFoundElementInDataSourceException {
+        Optional<Account> optionalAccount = accountRepo.findById(account.getId());
+        if (!optionalAccount.isPresent())
+            throw new NotFoundElementInDataSourceException("Account by ID not found in database.");
+        Account accountFromDB = optionalAccount.get();
+        if (!accountFromDB.getPassword().equals(account.getPassword()))
+            account.setPassword(passwordEncoder.encode(account.getPassword()));
+        if (!accountFromDB.getUsername().equals(account.getUsername()))
+            account.setUsername(account.getUsername());
+        Set<String> possibleRoleNames = Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
+        Set<Role> newRoles = new TreeSet<>();
+        for (String key : setRoles){
+            if (possibleRoleNames.contains(key))
+                newRoles.add(Role.valueOf(key));
+        }
+        account.setRoles(newRoles);
+        accountRepo.save(account); //Существующая запись обновится
+        return account;
+    }
 
 }
